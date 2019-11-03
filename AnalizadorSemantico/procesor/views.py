2529f74b7a2d1882807a12f524499634fe565097
anthonyOviedo <antony-08@hotmail.com>
2019-11-03 01:31:40 -0600

    web comit

--- AnalizadorSemantico/procesor/views.py
+++ AnalizadorSemantico/procesor/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+# crear una interfaz grafica para subir archivo.txt para ser analizado.
+
 
 def home(request):
     return HttpResponse("home Page")
